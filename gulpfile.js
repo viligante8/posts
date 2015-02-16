63d1576ae9553e9131f9849c9c639d7d4de94093
Vito Pistelli <vito.pistelli@careerbuilder.com>
2015-02-15 19:45:22 -0500

    added gulp uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var gulp = require('gulp')
 var concat = require('gulp-concat')
+var uglify = require('gulp-uglify')
 
 
 gulp.task('js', function(){
     gulp.src(['module.js', 'ng/**/*.js'])
         .pipe(concat('app.js'))
+        .pipe(uglify())
         .pipe(gulp.dest('assets'))
 })
-
